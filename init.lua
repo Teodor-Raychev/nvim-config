e04ff751dd70bd5689419e75879fec2b7b16f7a3
Teodor-Raychev <teodor.raychev@mentormate.com>
2024-03-29 19:48:13 +0200

    Added transparent theme

--- init.lua
+++ init.lua
@@ -2,12 +2,3 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 require("config.lazy")
--- try transparent:
--- function Transparent(color)
---   color = color or "tokyonight-storm"
---   vim.cmd.colorscheme(color)
---   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
---   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
---   -- vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
--- end
--- Transparent()
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,19 +9,31 @@
 --   end,
 -- }
 
+-- return {
+--   -- Theme NeoTokyo
+--   "tokyonight.nvim",
+--   lazy = true,
+--   priority = 1000,
+--   opts = {
+--     transparent = true,
+--     styles = {
+--       sidebars = "transparent",
+--       floats = "transparent",
+--     },
+--   },
+--   config = function()
+--     vim.cmd([[colorscheme tokyonight-storm]])
+--   end,
+-- }
 return {
-  -- Theme NeoTokyo
-  "tokyonight.nvim",
+  "folke/tokyonight.nvim",
   lazy = true,
-  priority = 1000,
   opts = {
     transparent = true,
+    style = "storm",
     styles = {
       sidebars = "transparent",
       floats = "transparent",
     },
   },
-  config = function()
-    vim.cmd([[colorscheme tokyonight-storm]])
-  end,
 }
